758f181f0128132edb563e14eb6ee848944788ed
luisec-2 <luiscue299@gmail.com>
2026-03-18 13:56:13 -0400

    Diagonal Movement Code Change

--- Assets/Scripts/MainPlayerCode.cs
+++ Assets/Scripts/MainPlayerCode.cs
@@ -4,40 +4,49 @@ using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 
 {
-public Rigidbody2D PRB;
-public float WalkSpeed = 5f;
+    public Rigidbody2D PRB;
+    public float WalkSpeed = 10f;
+    private Vector2 movement;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        float moveX = 0f;
+        float moveY = 0f;
+
         if (Input.GetKey(KeyCode.W))
         {
-            PRB.linearVelocity = new Vector2(0, WalkSpeed);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            PRB.linearVelocity = new Vector2(0, -WalkSpeed);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            PRB.linearVelocity = new Vector2(WalkSpeed, 0);
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            PRB.linearVelocity = new Vector2(-WalkSpeed, 0);
-        }
-        else
-        {
-            PRB.linearVelocity = new Vector2(0, 0);
+            moveY = 1f;
         }
 
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveY = -1f;
+        }
 
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveX = 1f;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveX = -1f;
+        }
+
+        movement = new Vector2(moveX, moveY).normalized;
     }
-    
+
+    void FixedUpdate()
+    {
+        PRB.linearVelocity = movement * WalkSpeed;
+    }
 }
+
+
