00363599701f2badae89686080a93055602fb14a
luisec-2 <luiscue299@gmail.com>
2026-03-22 18:28:58 -0400

    Button & Gate Talk and Work

--- Assets/Scripts/BScript.cs
+++ Assets/Scripts/BScript.cs
@@ -7,55 +7,55 @@ using UnityEngine.SceneManagement;
 
 public class BScript : MonoBehaviour
 {
-    public static int Gscore = 0;
+    public static int Gscore;
     public GameObject gate;
-    private bool Pressed = false;
-    
-        public void OnTriggerEnter(Collider player)
+    private bool Pressed;
+
+    public void OnTriggerEnter2D(Collider2D player)
+    {
+        Debug.Log("CHECK -> My name: [" + gameObject.name + "] | Player Tag: [" + player.tag + "]");
+        if (Pressed) return;
+
+        if (gameObject.name == "TButton" && player.CompareTag("PlayerT"))
         {
-            Debug.Log(gameObject.name + "touched" + player.gameObject.name);
-            if (Pressed) return;
-            
-            if (gameObject.name == "Tbutton" && player.CompareTag("PlayerT"))
-            {
-                Pressed = true;
-                Gscore++;
-                Debug.Log("GScore:1 " + Gscore);
-            }
-
-            else if (gameObject.name == "Cbutton" && player.CompareTag("PlayerC"))
-            {
-                Pressed = true;
-                Gscore++;
-                Debug.Log("GScore:2 " + Gscore);
-            }
-
-            else if (gameObject.name == "Sbutton" && player.CompareTag("PlayerS"))
-            {
-                Pressed = true;
-                Gscore++; 
-                Debug.Log("GScore:3 " + Gscore);
-            }
-
-            if (Gscore >= 3)
-            {
-                if (gate != null)
-                {
-                    Destroy(gate);
-                }    
-            }
-        
-
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-
+            Pressed = true;
+            Gscore++;
+            Debug.Log("GScore:1 " + Gscore);
         }
 
-        // Update is called once per frame
-        void Update()
+        else if (gameObject.name == "CButton" && player.CompareTag("PlayerC"))
         {
-                
+            Pressed = true;
+            Gscore++;
+            Debug.Log("GScore:2 " + Gscore);
+        }
+
+        else if (gameObject.name == "SButton" && player.CompareTag("PlayerS"))
+        {
+            Pressed = true;
+            Gscore++;
+            Debug.Log("GScore:3 " + Gscore);
+        }
+
+        if (Gscore >= 3)
+        {
+            if (gate != null)
+            {
+                Destroy(gate);
+            }
         }
     }
+
+// Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       // Gscore = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
+
